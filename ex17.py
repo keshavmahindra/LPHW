7b8ed8662bdae7f64be958622e4d5a28b87fc372
Keshav Mahindra <keshavmahindra@gmail.com>
2017-02-05 23:13:27 +1300

    Update LPHW

--- ex17.py
+++ ex17.py
@@ -10,12 +10,15 @@ print "Copying from %s to %s" % (from_file, to_file)
 in_file = open(from_file)
 indata = in_file.read()
 
+# Printing number of bytes in the file
 print "The input file is %d bytes long" % len(indata)
 
+# exists() is used to check if file with that name does exist
 print "Does the output file exist? %r" % exists(to_file)
 print "Ready, hit RETURN to continue, CTRL-C to abort."
 raw_input()
 
+# Specifying w when opening the file because it will be written to
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
@@ -24,3 +27,8 @@ print "Alright, all done."
 # Close both files
 out_file.close()
 in_file.close()
+
+# Syntax for creating txt file
+# echo "This is a text file" test.text
+# Syntax for opening txt file
+# cat test.txt
