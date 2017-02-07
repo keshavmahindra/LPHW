705acfc35241ea360b0ad8aa4fb666f6143daf5e
Keshav Mahindra <keshavmahindra@gmail.com>
2017-02-07 22:58:13 +1300

    Update LPHW

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1 is at 1 and is a python
+# The third (3rd) animal is at 2 and is a peacock
+# The first (1st) animal is at 0 and is a bear
+# The animal at 3 is at 3 and is a kangaroo
+# The fift (5th) animal is at 4 and is a whale
+# The animal at 2 is at 2 and is a peacock
+# The sixth (6th) animal is at 5 and is a platypus
+# The animal at 4 is at 4 and is whale
